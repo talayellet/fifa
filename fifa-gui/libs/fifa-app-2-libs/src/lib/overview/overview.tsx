bbf703726561c10a3650cd00c3ffaac025ca700a
Ayellet Tal <a.tal@f5.com>
2022-02-26 15:28:18 +0200

    feature: installs dependencies

--- fifa-gui/libs/fifa-app-2-libs/src/lib/overview/overview.tsx
+++ fifa-gui/libs/fifa-app-2-libs/src/lib/overview/overview.tsx
@@ -1,5 +1,23 @@
 import { ReactElement } from 'react';
+import Plot from 'react-plotly.js';
 
 export const Overview = (): ReactElement => {
-  return <div>BBB</div>;
+  return (
+    <div>
+      <Plot
+        data={[
+          {
+            x: [1, 2, 3, 4, 5],
+            y: [1, 6, 3, 6, 1],
+            mode: 'markers',
+            type: 'scatter',
+            name: 'Team A',
+            text: ['A-1', 'A-2', 'A-3', 'A-4', 'A-5'],
+            marker: { size: 12 },
+          },
+        ]}
+        layout={{ width: 320, height: 240, title: 'A Fancy Plot' }}
+      />
+    </div>
+  );
 };
